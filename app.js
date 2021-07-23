7fd7e4fb0c76ae0915bafe144a1538c2cb1bbf65
Sarthak <sarthakarora1503@gmail.com>
2021-07-23 20:34:11 +0530

    get at / route added

--- app.js
+++ app.js
@@ -23,6 +23,11 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 
+
+app.get('/',(req,res)=> {
+    res.status(200).send('Welcome to API Operations!');
+});
+
 app.use('/auth',routeLogin);
 app.use('/delivery',routeDelivery);
 app.use('/handover',routeHandover);
