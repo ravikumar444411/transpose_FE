6c34c6205265cc0c4eb265133c8b3fa5ba355f42
Deepanshu Chaudhary <43883098+deepanshusejwar@users.noreply.github.com>
2021-07-26 22:21:21 +0530

    Update App.js

--- App.js
+++ App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import { NativeBaseProvider, Box } from 'native-base';
 import Login from './src/Login';
 import Pickup from './src/Pickup';
+import Dashboard from './src/Dashboard';
+
 
 function App() {
   return (
     <NativeBaseProvider>
       <Pickup/>
+    <Dashobard/>
     </NativeBaseProvider>
   );
 }
