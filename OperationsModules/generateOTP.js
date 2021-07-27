2e7cc6263b541efb385ba45675a7810514e33e21
Sarthak <sarthakarora1503@gmail.com>
2021-07-28 00:57:41 +0530

    otp generating easily

--- OperationsModules/generateOTP.js
+++ OperationsModules/generateOTP.js
@@ -2,17 +2,18 @@
 
 function generateOTP() {
     let otp = Math.floor(Math.random()*10000000);
-    let OTP = otp.toString();
-    let temp = "";
+    // let OTP = otp.toString();
+    // let temp = "";
 
-    for(let i=0;i<(6-OTP.length);i++) {
-        temp += "0";
-    }
+    // for(let i=0;i<(6-OTP.length);i++) {
+    //     temp += "0";
+    // }
 
-    let completeOtp = temp + OTP;
-    console.log(completeOtp);
+    // let completeOtp = temp + OTP;
+    // console.log(completeOtp);
 
-    return completeOtp;
+    // return completeOtp;
+    return otp;
 }
 
 module.exports = generateOTP;
